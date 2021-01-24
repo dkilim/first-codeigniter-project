709eb1749634b2266b267290c2671a90f545d556
dkilim <43480145+dkilim@users.noreply.github.com>
2021-01-24 18:19:18 +0100

    vremenska zona

--- app/Config/App.php
+++ app/Config/App.php
@@ -100,7 +100,9 @@ class App extends BaseConfig
 	| dates with the date helper, and can be retrieved through app_timezone()
 	|
 	*/
-	public $appTimezone = 'America/Chicago';
+	// public $appTimezone = 'America/Chicago';
+	public $appTimezone = 'Europe/Sarajevo';
+	
 
 	/*
 	|--------------------------------------------------------------------------
